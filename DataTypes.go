7f158a458b0136fb5f86b63f62962605d1e52c57
Subhesh Kumar <82794288+Subhesh-kumar@users.noreply.github.com>
2023-01-27 21:26:32 +0530

    Add files via upload

---
+++ DataTypes.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	//var Variable_name datatype
+	var data int
+	data = 10
+	data1 := 100
+	//data := false //wrong asignment value type
+	data = 1000
+	Data := 1000
+
+	fmt.Println(data)
+	fmt.Println(data1)
+	fmt.Println(Data)
+	name := "subhesh"
+	fmt.Println(name)
+
+}
